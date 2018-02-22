6194683cc8a89879e72e94695f7e91187e60f6a7
Léon Smit <lee_smit@live.nl>
2018-02-22 21:58:09 +0100

    Update RNHexagonView.swift

--- ios/RNHexagonView.swift
+++ ios/RNHexagonView.swift
@@ -102,16 +102,15 @@ class RNHexagonView: UIView {
     func setupHexagonView(view: UIView) {
         let lineWidth = self._borderWidth
         let borderColor = self._borderColor
-        let rotationOffset = self._isHorizontal ? CGFloat(M_PI) : CGFloat(M_PI / 2.0)
         let cornerRadius = self._cornerRadius
         
-        let path = roundedPolygonPath(rect: view.bounds, lineWidth: lineWidth, sides: 6, cornerRadius: cornerRadius, rotationOffset: rotationOffset)
+        let path = UIBezierPath(polygonIn: view.bounds, sides: 6, lineWidth: lineWidth, cornerRadius: cornerRadius)
         
         let mask = CAShapeLayer()
         mask.path = path.cgPath
         mask.fillColor = UIColor.white.cgColor
         (self.layer as! CAShapeLayer).mask = mask
-        
+      
         borderLayer.removeFromSuperlayer()
         
         (self.layer as! CAShapeLayer).path = path.cgPath
@@ -123,52 +122,59 @@ class RNHexagonView: UIView {
         borderLayer.fillColor = nil
         (self.layer as! CAShapeLayer).addSublayer(borderLayer)
     }
-    
-    func roundedPolygonPath(rect: CGRect, lineWidth: CGFloat, sides: NSInteger, cornerRadius: CGFloat, rotationOffset: CGFloat = 0)
-        -> UIBezierPath {
-            let path = UIBezierPath()
-            let theta: CGFloat = CGFloat(2.0 * M_PI) / CGFloat(sides) // How much to turn at every corner
-            //      let offset: CGFloat = cornerRadius * tan(theta / 2.0)     // Offset from which to start rounding corners
-            let width = min(rect.size.width, rect.size.height)        // Width of the square
-            
-            let center = CGPoint(x: rect.origin.x + width / 2.0, y: rect.origin.y + (self._isHorizontal ? 0 : width) / 2.0)
-            
-            // Radius of the circle that encircles the polygon
-            // Notice that the radius is adjusted for the corners, that way the largest outer
-            // dimension of the resulting shape is always exactly the width - linewidth
-            let radius = (width - lineWidth + cornerRadius - (cos(theta) * cornerRadius)) / 2.0
-            
-            // Start drawing at a point, which by default is at the right hand edge
-            // but can be offset
-            var angle = CGFloat(rotationOffset)
-            
-            let corner = CGPoint(x: center.x + (radius - cornerRadius) * cos(angle), y: center.y + (radius - cornerRadius) * sin(angle))
-            path.move(to: CGPoint(x: corner.x + cornerRadius * cos(angle + theta), y: corner.y + cornerRadius * sin(angle + theta)))
-            
-            for _ in 0 ..< sides {
-                angle += theta
-                
-                let corner = CGPoint(x: center.x + (radius - cornerRadius) * cos(angle), y: center.y + (radius - cornerRadius) * sin(angle))
-                let tip = CGPoint(x: center.x + radius * cos(angle), y: center.y + radius * sin(angle))
-                let start = CGPoint(x: corner.x + cornerRadius * cos(angle - theta), y: corner.y + cornerRadius * sin(angle - theta))
-                let end = CGPoint(x: corner.x + cornerRadius * cos(angle + theta), y: corner.y + cornerRadius * sin(angle + theta))
-                
-                path.addLine(to: start)
-                path.addQuadCurve(to: end, controlPoint: tip)
-            }
-            
-            path.close()
-            
-            // Move the path to the correct origins
-            let bounds = path.bounds
-            let transform = CGAffineTransform(translationX: -bounds.origin.x + rect.origin.x + lineWidth / 2.0,
-                                              y: -bounds.origin.y + rect.origin.y + lineWidth / 2.0)
-            path.apply(transform)
-            
-            return path
-    }
-    
+
     override class var layerClass: AnyClass {
         return CAShapeLayer.self
     }
+  
+}
+extension UIBezierPath {
+  
+  /// Create UIBezierPath for regular polygon with rounded corners
+  ///
+  /// - parameter rect:            The CGRect of the square in which the path should be created.
+  /// - parameter sides:           How many sides to the polygon (e.g. 6=hexagon; 8=octagon, etc.).
+  /// - parameter lineWidth:       The width of the stroke around the polygon. The polygon will be inset such that the stroke stays within the above square. Default value 1.
+  /// - parameter cornerRadius:    The radius to be applied when rounding the corners. Default value 0.
+  
+  convenience init(polygonIn rect: CGRect, sides: Int, lineWidth: CGFloat = 1, cornerRadius: CGFloat = 0) {
+    self.init()
+    
+    let theta = 2 * CGFloat.pi / CGFloat(sides)                        // how much to turn at every corner
+    let offset = cornerRadius * tan(theta / 2)                  // offset from which to start rounding corners
+    let squareWidth = min(rect.size.width, rect.size.height)    // width of the square
+    
+    // calculate the length of the sides of the polygon
+    
+    var length = squareWidth - lineWidth
+    if sides % 4 != 0 {                                         // if not dealing with polygon which will be square with all sides ...
+      length = length * cos(theta / 2) + offset / 2           // ... offset it inside a circle inside the square
+    }
+    let sideLength = length * tan(theta / 2)
+    
+    // start drawing at `point` in lower right corner, but center it
+    
+    var point = CGPoint(x: rect.origin.x + rect.size.width / 2 + sideLength / 2 - offset, y: rect.origin.y + rect.size.height / 2 + length / 2)
+    var angle = CGFloat.pi
+    move(to: point)
+    
+    // draw the sides and rounded corners of the polygon
+    
+    for _ in 0 ..< sides {
+      point = CGPoint(x: point.x + (sideLength - offset * 2) * cos(angle), y: point.y + (sideLength - offset * 2) * sin(angle))
+      addLine(to: point)
+      
+      let center = CGPoint(x: point.x + cornerRadius * cos(angle + .pi / 2), y: point.y + cornerRadius * sin(angle + .pi / 2))
+      addArc(withCenter: center, radius: cornerRadius, startAngle: angle - .pi / 2, endAngle: angle + theta - .pi / 2, clockwise: true)
+      
+      point = currentPoint
+      angle += theta
+    }
+    
+    close()
+    
+    self.lineWidth = lineWidth           // in case we're going to use CoreGraphics to stroke path, rather than CAShapeLayer
+    lineJoinStyle = .round
+  }
+  
 }
